16d4144ceeaaffaffd274741766e9bd3a5cba42b
Magnar Sveen <magnars@gmail.com>
2014-01-21 14:24:36 +0100

    Første runde på ekstra innhold. Enkle anbefalinger.

--- src/kodemaker_no/pages/person_pages.clj
+++ src/kodemaker_no/pages/person_pages.clj
@@ -1,16 +1,27 @@
 (ns kodemaker-no.pages.person-pages)
 
+(defn- render-recommendation [rec]
+  (list [:h3 [:a {:href (:url rec)} (:title rec)]]
+        [:p (:blurb rec)]))
+
+(defn- render-recommendations [person recs]
+  (list [:h2 (str (:first-name person) "s Anbefalinger")]
+        (map render-recommendation recs)))
+
 (defn- person-page [person]
   {:title (:full-name person)
    :illustration (-> person :photos :half-figure)
    :lead [:p (:description person)]
    :aside [:div.tight
-           [:h5 (:full-name person)]
+           [:h4 (:full-name person)]
            [:p
             (:title person) "<br>"
             [:span.nowrap (:phone-number person)] "<br>"
             [:a {:href (str "mailto:" (:email-address person))}
-             (:email-address person)]]]})
+             (:email-address person)]]]
+   :body (list
+          (when-let [recs (:recommendations person)]
+            (render-recommendations person recs)))})
 
 (defn person-pages [people]
   (into {} (map (juxt :url #(partial person-page %)) people)))
--- test/kodemaker_no/pages/person_pages_test.clj
+++ test/kodemaker_no/pages/person_pages_test.clj
@@ -1,14 +1,24 @@
 (ns kodemaker-no.pages.person-pages-test
   (:require [kodemaker-no.pages.person-pages :refer :all]
-            [midje.sweet :refer :all]))
+            [midje.sweet :refer :all]
+            [hiccup.core :refer [html]]
+            [kodemaker-no.homeless :refer [hiccup-find]]))
 
-(def pages (person-pages [{:url "/magnar/"
-                           :full-name "Magnar Sveen"
-                           :title "Framsieutvikler"
-                           :photos {:half-figure "/photos/magnars/half-figure.jpg"}
-                           :description "The description"
-                           :phone-number "+47 918 56 425"
-                           :email-address "magnar@kodemaker.no"}]))
+(def pages
+  (person-pages
+   [{:url "/magnar/"
+     :full-name "Magnar Sveen"
+     :first-name "Magnar"
+     :title "Framsieutvikler"
+     :photos {:half-figure "/photos/magnars/half-figure.jpg"}
+     :description "The description"
+     :phone-number "+47 918 56 425"
+     :email-address "magnar@kodemaker.no"
+
+     :recommendations [{:title "Anbefaling 1"
+                        :blurb "Denne er bra."
+                        :url "http://example.com"
+                        :tech [:clojure]}]}]))
 
 (def page ((pages "/magnar/")))
 
@@ -17,9 +27,13 @@
 (fact (-> page :lead) => [:p "The description"])
 
 (fact (-> page :aside) => [:div.tight
-                           [:h5 "Magnar Sveen"]
+                           [:h4 "Magnar Sveen"]
                            [:p
                             "Framsieutvikler" "<br>"
                             [:span.nowrap "+47 918 56 425"] "<br>"
                             [:a {:href "mailto:magnar@kodemaker.no"}
                              "magnar@kodemaker.no"]]])
+
+(fact (->> page :body html) => (html [:h2 "Magnars Anbefalinger"]
+                                     [:h3 [:a {:href "http://example.com"} "Anbefaling 1"]]
+                                     [:p "Denne er bra."]))
