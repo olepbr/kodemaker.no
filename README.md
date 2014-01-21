92ce617647754584067550056fc8440f5956338f
Magnar Sveen <magnars@gmail.com>
2014-01-21 09:17:15 +0100

    Mer jobb med datastruktur

--- README.md
+++ README.md
@@ -29,12 +29,32 @@ datastrukturen blir omtrent slik, vil jeg tro:
    :description Str
    (optional-key :administration?) Boolean
 
-   (optional-key :blogs) [{:name Str
+   :phone-number Str
+   :email-address Str
+
+   :presence {(optional-key :cv) Str ;; Kodemaker cv id
+              (optional-key :twitter) Str  ;; username
+              (optional-key :linkedin) Str ;; path to public profile
+              (optional-key :stackoverflow) Str ;; path to public profile
+              (optional-key :github) Str ;; username
+              (optional-key :coderwall) Str} ;; username
+
+   (optional-key :tech) {:favorites-at-the-moment [Keyword]
+                         (optional-key :want-to-learn-more) [Keyword]}
+
+   (optional-key :blogs) [{:id Keyword
+                           :name Str
                            :url Str
-                           :posts [{:url Str
-                                    :title Str
-                                    :blurb Str
-                                    :tech [Keyword]}]}]
+                           :theme Str ;; very short
+                           :tech [Keyword]}]
+
+   (optional-key :blog-posts) [{:url Str
+                                :title Str
+                                :blurb Str
+                                :tech [Keyword]
+                                :blog (either Keyword ;; :id from :blogs
+                                              {:name Str
+                                               :url Str})}]
 
    (optional-key :recommendations) [{:url Str
                                      :title Str
@@ -48,6 +68,7 @@ datastrukturen blir omtrent slik, vil jeg tro:
 
    (optional-key :endorsements) [{:author Str
                                   :quote Str
+                                  (optional-key :title) Str
                                   (optional-key :project) Keyword
                                   (optional-key :photo) Str}]})
 ```
--- src/kodemaker_no/validate.clj
+++ src/kodemaker_no/validate.clj
@@ -1,5 +1,5 @@
 (ns kodemaker-no.validate
-  (:require [schema.core :refer [optional-key validate Str Keyword Num]]))
+  (:require [schema.core :refer [optional-key validate either Str Keyword Num]]))
 
 (def Person
   {:id Keyword
@@ -9,12 +9,32 @@
    :description Str
    (optional-key :administration?) Boolean
 
-   (optional-key :blogs) [{:name Str
+   :phone-number Str
+   :email-address Str
+
+   :presence {(optional-key :cv) Str ;; Kodemaker cv id
+              (optional-key :twitter) Str  ;; username
+              (optional-key :linkedin) Str ;; path to public profile
+              (optional-key :stackoverflow) Str ;; path to public profile
+              (optional-key :github) Str ;; username
+              (optional-key :coderwall) Str} ;; username
+
+   (optional-key :tech) {:favorites-at-the-moment [Keyword]
+                         (optional-key :want-to-learn-more) [Keyword]}
+
+   (optional-key :blogs) [{:id Keyword
+                           :name Str
                            :url Str
-                           :posts [{:url Str
-                                    :title Str
-                                    :blurb Str
-                                    :tech [Keyword]}]}]
+                           :theme Str ;; very short
+                           :tech [Keyword]}]
+
+   (optional-key :blog-posts) [{:url Str
+                                :title Str
+                                :blurb Str
+                                :tech [Keyword]
+                                :blog (either Keyword ;; :id from :blogs
+                                              {:name Str
+                                               :url Str})}]
 
    (optional-key :recommendations) [{:url Str
                                      :title Str
@@ -28,6 +48,7 @@
 
    (optional-key :endorsements) [{:author Str
                                   :quote Str
+                                  (optional-key :title) Str
                                   (optional-key :project) Keyword
                                   (optional-key :photo) Str}]})
 
