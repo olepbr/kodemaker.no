ed2305486c2f83e06eeae498ca9d006b5308779e
Magnar Sveen <magnars@gmail.com>
2014-01-25 22:52:19 +0100

    Prosjektsider: Våre folk på saken

--- src/kodemaker_no/cultivate/projects.clj
+++ src/kodemaker_no/cultivate/projects.clj
@@ -1,13 +1,30 @@
 (ns kodemaker-no.cultivate.projects
   (:require [kodemaker-no.cultivate.util :as util]
-            [kodemaker-no.homeless :refer [update-vals]]))
+            [kodemaker-no.homeless :refer [update-vals assoc-in-unless]]
+            [clojure.string :as str]))
 
 (defn- add-url [project]
   (assoc project :url (util/url project)))
 
+(defn- get-with-person-info [key]
+  (fn [person]
+    (->> (key person)
+         (map #(-> %
+                   (assoc :name (str/join " " (:name person)))
+                   (assoc :url (util/url person))
+                   (assoc :thumb (str "/photos/people/" (-> person :id str (subs 1)) "/side-profile.jpg")))))))
+
+(defn- add-people [content project]
+  (assoc-in-unless project [:people] empty?
+                   (->> (:people content)
+                        vals
+                        (mapcat (get-with-person-info :projects))
+                        (filter #(= (:id project) (:id %))))))
+
 (defn- cultivate-project [content project]
   (->> project
-       add-url))
+       add-url
+       (add-people content)))
 
 (defn cultivate-projects [content]
   (update-vals (:projects content) (partial cultivate-project content)))
--- src/kodemaker_no/pages/project_pages.clj
+++ src/kodemaker_no/pages/project_pages.clj
@@ -1,9 +1,20 @@
 (ns kodemaker-no.pages.project-pages)
 
+(defn- render-person [person]
+  [:div.media
+   [:a.img.thumb.mts {:href (:url person)}
+    [:img {:src (:thumb person)}]]
+   [:div.bd
+    [:h4.mtn (:name person)]
+    [:p (:description person)]]])
+
 (defn- project-page [project]
   {:title (:name project)
    :illustration (:logo project)
-   :lead [:p (:description project)]})
+   :lead [:p (:description project)]
+   :body (list
+          [:h2 "Våre folk på saken"]
+          (map render-person (:people project)))})
 
 (defn project-pages [projects]
   (into {} (map (juxt :url #(partial project-page %)) projects)))
