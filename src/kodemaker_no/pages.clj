88a0c6fce8eba39ff01856f81fcf138ef1182c17
Magnar Sveen <magnars@gmail.com>
2014-04-04 08:09:50 +0200

    Fjern utdaterte sider

--- src/kodemaker_no/pages.clj
+++ src/kodemaker_no/pages.clj
@@ -3,7 +3,6 @@
             [kodemaker-no.pages.blog-pages :refer [blog-post-pages blog-page]]
             [kodemaker-no.pages.index-page :refer [index-page]]
             [kodemaker-no.pages.form-page :refer [form-page]]
-            [kodemaker-no.pages.people-page :refer [all-people]]
             [kodemaker-no.pages.person-pages :refer [person-pages]]
             [kodemaker-no.pages.project-pages :refer [project-pages]]
             [kodemaker-no.pages.references-page :refer [references-page]]
@@ -13,7 +12,6 @@
 (defn general-pages [content]
   {"/" (partial index-page (vals (:people content)) (:index content))
    "/skjema/" (partial form-page (:index content))
-   "/mennesker/" (partial all-people (vals (:people content)))
    "/referanser/" (partial references-page (vals (:projects content)))
    "/blogg/" (partial blog-page (vals (:blog-posts content)))})
 
