46479d3b0f66b2f3d6db738d21c0c33783e68723
Magnar Sveen <magnars@gmail.com>
2014-05-13 14:48:21 +0200

    Design: Kontaktskjema

--- src/kodemaker_no/pages/form_page.clj
+++ src/kodemaker_no/pages/form_page.clj
@@ -4,15 +4,23 @@
 (defn form-page []
   {:title "Hva kan vi hjelpe deg med?"
    :body (list
+          [:p
+           "For å hjelpe oss plukke ut prosjekter vi har tro på, vil vi at du
+            fyller ut dette prosjektskjemaet. Vi leser over det og tar kontakt med
+            deg så snart vi har mulighet. Stort sett vil du høre fra oss innen to
+            arbeidsdager."]
           [:form.form.mod {:action "/send-mail"
                            :method "POST"}
-           [:label "Prosjektbeskrivelse:"]
-           [:textarea.input {:rows 4, :name "tekst"}]
-           [:label "Omtrentlig omfang:"]
-           [:input.input {:type "text", :name "omfang"}]
-           [:label "Ønsket oppstart:"]
-           [:input.input {:type "text", :name "oppstart"}]
-           [:label "Kontaktinformasjon:"]
+           [:label "Skriv litt om prosjektet ditt"]
+           [:textarea.input {:rows 8, :name "tekst"}]
+           [:div.line
+            [:div.unit.r-1of2
+             [:label "Når ønsker du å starte?"]
+             [:input.input {:type "text", :name "oppstart"}]]
+            [:div.lastUnit
+             [:label "Hvilket omfang ser du for deg?"]
+             [:input.input {:type "text", :name "omfang"}]]]
+           [:label "Hvordan får vi tak i deg?"]
            [:input.input {:type "text", :name "kontakt"}]
            [:div
             [:button.btn {:type "submit"} "Send skjema"]]])})
