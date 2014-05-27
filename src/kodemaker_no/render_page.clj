e4db5d7feefb905a8c9b1a34b1c124f417380275
Magnar Sveen <magnars@gmail.com>
2014-05-27 06:08:38 +0200

    Fjern siden med "læring", den er ikke klar
    @steni, foreslår at vi utvikler denne ferdig i en branch, så vi har
    muligheten til å gå live hvis vi ikke får sidene ferdige.

--- src/kodemaker_no/render_page.clj
+++ src/kodemaker_no/render_page.clj
@@ -37,7 +37,6 @@
     [:div#ow ;; outer-wrapper for off-canvas menu
      [:div#ocm ;; off-canvas menu
       [:div.bd
-       [:div.ocm-item [:a {:href "/kurs/"} "Læring"]]
        [:div.ocm-item [:a {:href "/blogg/"} "Blogg"]]
        [:div.ocm-item [:a {:href "/skjema/"} "Ta kontakt"]]]]
      [:div#iw ;; inner-wrapper for off-canvas menu
@@ -46,7 +45,6 @@
         [:div.bd
          [:div#ocb.mod [:span] [:span] [:span]]
          [:div.mod.menu
-          [:a {:href "/kurs/"} "Læring"]
           [:a {:href "/blogg/"} "Blogg"]
           [:a {:href "/skjema/"} "Ta kontakt"]]
          [:h1#logo.hn
