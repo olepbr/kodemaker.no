0dfb515ec6e09c1baebe787c70d3dd5f4c975b87
Magnar Sveen <magnars@gmail.com>
2014-06-12 13:16:19 +0200

    Sett opp redirect fra nye.kodemaker til www.kodemaker

--- provisioning/files/mail-config-sample.js
+++ provisioning/files/mail-config-sample.js
@@ -1,6 +1,6 @@
 module.exports = {
   from: "web@kodemaker.no",
   to: "kontakt@kodemaker.no",
-  subject: "Forespørsel fra nye.kodemaker.no",
+  subject: "Forespørsel fra kodemaker.no",
   apiUser: "api:key-secret"
 };
