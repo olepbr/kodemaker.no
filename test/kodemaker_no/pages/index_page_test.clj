bf7daf2b2abe5a2f1d18cd0a64a8ddcd9372c279
Magnar Sveen <magnars@gmail.com>
2014-12-09 07:00:49 +0100

    Fix frontpage tests

--- test/kodemaker_no/pages/index_page_test.clj
+++ test/kodemaker_no/pages/index_page_test.clj
@@ -15,5 +15,5 @@
       (->> (index-page [{:full-name "Magnar Sveen",   :administration? false, :start-date "2007-03-01"}
                         {:full-name "Finn J Johnsen", :administration? false, :start-date "2008-10-01"}
                         {:full-name "Kolbjørn Jetne", :administration? true,  :start-date "2007-04-01"}])
-           :body (hiccup-find :.linkish) (map second))
+           :sections first :body (hiccup-find :.linkish) (map second))
       => ["Finn J Johnsen" "Magnar Sveen"])
