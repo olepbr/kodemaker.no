3825a481a6ba00ed654ced58de3a7ad80d54f551
Magnar Sveen <magnars@gmail.com>
2016-06-10 06:37:11 +0200

    Oppdater avhengigheter

--- project.clj
+++ project.clj
@@ -3,35 +3,35 @@
   :url "http://nye.kodemaker.no"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [optimus "0.14.2"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [optimus "0.18.5"]
                  [optimus-img-transform "0.2.0"]
-                 [stasis "2.1.1" :exclusions [org.clojure/clojure]]
-                 [ring "1.2.1"]
-                 [hiccup "1.0.4"]
-                 [enlive "1.1.5"]
+                 [stasis "2.3.0" :exclusions [org.clojure/clojure]]
+                 [ring "1.4.0"]
+                 [hiccup "1.0.5"]
+                 [enlive "1.1.6"]
                  [mapdown "0.2.1"]
                  [me.raynes/cegdown "0.1.1"]
-                 [prismatic/schema "0.2.0"]
-                 [clj-time "0.6.0"]
-                 [org.clojure/core.memoize "0.5.6"]
+                 [prismatic/schema "1.1.1"]
+                 [clj-time "0.11.0"]
+                 [org.clojure/core.memoize "0.5.9"]
                  [clygments "0.1.1"]
-                 [prone "0.6.1"]]
+                 [prone "1.1.1"]]
   :jvm-opts ["-Xmx768M"
              "-Djava.awt.headless=true"]
   :ring {:handler kodemaker-no.web/app
          :port 3333}
   :aliases {"build-site" ["run" "-m" "kodemaker-no.web/export"]}
-  :profiles {:dev {:dependencies [[print-foo "0.4.2"]]
-                   :plugins [[lein-ring "0.8.10"]]
+  :profiles {:dev {:dependencies [[print-foo "1.0.2"]]
+                   :plugins [[lein-ring "0.9.7"]]
                    :source-paths ["dev" "config"]
                    :test-paths ^:replace []}
-             :test {:dependencies [[midje "1.6.3"]
-                                   [test-with-files "0.1.0"]
-                                   [flare "0.2.5"]]
+             :test {:dependencies [[midje "1.8.3"]
+                                   [test-with-files "0.1.1"]
+                                   [flare "0.2.9"]]
                     :injections [(require 'flare.midje)
                                  (flare.midje/install!)]
-                    :plugins [[lein-midje "3.1.3"]]
+                    :plugins [[lein-midje "3.2"]]
                     :source-paths ["config"]
                     :test-paths ["test"]
                     :resource-paths ["test/resources"]}})
