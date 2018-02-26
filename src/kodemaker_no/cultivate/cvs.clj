704e960f0cdaa466432657cb66c3e1eb7450243e
Christian Johansen <christian@cjohansen.no>
2018-02-26 23:50:08 +0100

    Kultivér CV-data

---
+++ src/kodemaker_no/cultivate/cvs.clj
@@ -0,0 +1,80 @@
+(ns kodemaker-no.cultivate.cvs
+  (:require [clojure.string :as str]
+            [clojure.walk :as walk]
+            [kodemaker-no.cultivate.util :as util]))
+
+(defn override [m ns]
+  (merge m
+         (->> (keys m)
+              (filter #(and (= (namespace %) ns) (contains? m (keyword (name %)))))
+              (map (fn [k] [(-> k name keyword) (k m)]))
+              (into {}))))
+
+(defn apply-cv-overrides [person]
+  (walk/postwalk #(if (map? %) (override % "cv") %) person))
+
+(defn cultivate-techs [person content]
+  (->> (concat (->> person :tech :using-at-work)
+               (->> person :innate-skills)
+               (->> (select-keys person [:side-projects
+                                         :blog-posts
+                                         :screencasts
+                                         :presentations
+                                         :open-source-projects
+                                         :open-source-contributions
+                                         :projects])
+                    vals
+                    (apply concat)
+                    (mapcat :tech)))
+       flatten
+       (group-by identity)
+       (map (fn [[k ks]] (merge (util/look-up-tech content k) {:count (count ks)})))
+       (sort-by :count)
+       reverse
+       (group-by :type)))
+
+(defn lookup-employer [employers project]
+  (assoc project :employer (employers (:employer project))))
+
+(def months ["Januar" "Februar" "Mars" "April" "Mai" "Juni" "Juli"
+             "August" "September" "Oktober" "November" "Desember"])
+
+(defn- date->ym [appearance]
+  (if (string? (:date appearance))
+    (let [[y m] (str/split (:date appearance) #"-")]
+      (->> (format "%s %s" (months (dec (Integer/parseInt m))) y)
+           (assoc appearance :year-month)))
+    appearance))
+
+(defn- cultivate-appearances [{:keys [presentations appearances]}]
+  (->> (concat presentations appearances)
+       (sort-by :date)
+       reverse
+       (map date->ym)))
+
+(defn- main-proglang [x]
+  (->> x
+       :tech
+       (filter #(= :proglang (:type %)))
+       first
+       :name))
+
+(defn- cultivate-open-source-contributions [{:keys [open-source-projects open-source-contributions]}]
+  (->> (concat (map #(assoc % :role :developer) open-source-projects)
+               (map #(assoc % :role :contributer) open-source-contributions))
+       (group-by main-proglang)))
+
+(defn cultivate-cv [person tech content]
+  (let [data ((:id person) (:people content))]
+    (-> person
+        apply-cv-overrides
+        (assoc :techs (cultivate-techs data content))
+        (update-in [:projects] #(map (partial lookup-employer (:employers content)) %))
+        (assoc :appearances (cultivate-appearances person))
+        (assoc :open-source-contributions (cultivate-open-source-contributions person)))))
+
+(defn cultivate-cvs [raw-content people tech]
+  (->> people
+       (filter #(-> % second :use-new-cv?))
+       (map (fn [[id person]] [id (cultivate-cv person tech raw-content)]))
+       (into {})))
---
+++ test/kodemaker_no/cultivate/cvs_test.clj
@@ -0,0 +1,126 @@
+(ns kodemaker-no.cultivate.cvs-test
+  (:require [kodemaker-no.cultivate.content-shells :as c]
+            [kodemaker-no.cultivate.cvs :as cvs]
+            [kodemaker-no.cultivate.people :as p]
+            [kodemaker-no.cultivate.tech :as t]
+            [kodemaker-no.validate :refer [validate-content]]
+            [midje.sweet :refer :all]))
+
+(def content
+  (c/content
+   {:people
+    {:christian
+     (c/person
+      {:id :christian
+       :name ["Christian" "Johansen"]
+       :use-new-cv? true
+       :tech {:using-at-work [:clojure :java]}
+       :innate-skills [:bash]
+       :description "Default beskrivelse"
+       :cv/description "CV-beskrivelse"
+       :recommendations [{:title "Esoteric topic"
+                          :blurb "Esoteric"
+                          :link {:url "http://lolcathost" :text "Se"}
+                          :tech [:haskell]}]
+       :side-projects [{:title "Sideprosjekt"
+                        :description "Test"
+                        :illustration "/prosjekt.png"
+                        :tech [:clojurescript :clojure]}]
+       :presentations [{:title "Foredrag"
+                        :blurb "Foredrag"
+                        :tech [:unix]
+                        :date "2017-01-02"
+                        :urls {:video "http://lolcathost"}}]
+       :appearances [{:title "Annet foredrag"
+                      :date "2017-02-01"
+                      :event "JavaZone"
+                      :tech [:zsh]}]
+       :open-source-projects [{:name "Sinon.JS"
+                               :url "https://sinonjs.org"
+                               :description "Sinon"
+                               :tech [:sinon :java]}]
+       :open-source-contributions [{:name "Buster.JS"
+                                    :url "https://busterjs.org"
+                                    :tech [:tdd :jira :clojure]}]
+       :projects [{:customer "Bank"
+                   :description "Beskrivelse"
+                   :years [2020]
+                   :tech [:java]
+                   :employer :kodemaker}
+                  {:customer "Bank"
+                   :cv/customer "Bank1"
+                   :description "Beskrivelse"
+                   :cv/description "Beskrivelse1"
+                   :years [2020]
+                   :tech [:java]
+                   :employer :kodemaker}]})
+
+     :kjetil
+     (c/person
+      {:id :kjetil
+       :name ["Kjetil" "Jørgensen-Dahl"]
+       :use-new-cv? true
+       :description "Default beskrivelse"})
+
+     :magnar
+     (c/person
+      {:id :magnar
+       :name ["Magnar" "Sveen"]
+       :use-new-cv? false})}
+
+    :tech-types {:java :proglang
+                 :jira :methodology
+                 :tdd :technique
+                 :bash :proglang
+                 :sinon :library
+                 :clojure :proglang
+                 :clojurescript :proglang
+                 :unix :os
+                 :haskell :proglang}
+
+    :employers {:kodemaker "Kodemaker AS"}}))
+
+(defn cultivate [content]
+  (let [raw (validate-content content)]
+    (cvs/cultivate-cvs raw (p/cultivate-people raw) (t/cultivate-techs raw))))
+
+(let [cvs (cultivate content)]
+  (fact
+   "New CVs are opt-in"
+   (map :id (vals cvs)) => [:christian :kjetil])
+
+  (fact
+   "Prefers cv/description when available"
+   (map :description (vals cvs)) => ["CV-beskrivelse" "Default beskrivelse"])
+
+  (fact
+   "Prefers cv overrides in projects"
+   (->> cvs :christian :projects (map #(select-keys % [:customer :description]))) =>
+   [{:description "Beskrivelse" :customer "Bank"}
+    {:description "Beskrivelse1" :customer "Bank1"}])
+
+  (fact
+   "Groups techs by type"
+   (->> cvs :christian :techs keys set) => #{:proglang :library :methodology :technique :os})
+
+  (fact
+   "Orders techs by most frequently referenced"
+   (->> cvs :christian :techs :proglang (map :id)) => [:java :clojure :clojurescript :bash])
+
+  (fact
+   "Compiles techs from all self-effort fields"
+   (->> cvs :christian :techs vals (apply concat) (map :id) set) =>
+   #{:java :clojure :clojurescript :bash :sinon :tdd :jira :unix})
+
+  (fact
+   "Resolves project employers"
+   (->> cvs :christian :projects (map :employer)) => ["Kodemaker AS" "Kodemaker AS"])
+
+  (fact
+   "Combines appearances with presentations and sorts by date"
+   (->> cvs :christian :appearances (map :title)) => ["Annet foredrag" "Foredrag"])
+
+  (fact
+   "Combines open source projects and contributions, and groups by programming language"
+   (->> cvs :christian :open-source-contributions keys) => ["Java" "Clojure"]
+   (->> cvs :christian :open-source-contributions vals (map #(map :name %))) => [["Sinon.JS"] ["Buster.JS"]]))
