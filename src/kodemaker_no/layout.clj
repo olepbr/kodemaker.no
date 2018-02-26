ca4158150c8ef660f15f76421605f36c1df416fb
Christian Johansen <christian@cjohansen.no>
2018-02-26 22:44:38 +0100

    Legg til Google Analytics og footer i CV-layout

--- src/kodemaker_no/layout.clj
+++ src/kodemaker_no/layout.clj
@@ -52,7 +52,18 @@
     [:link {:href (link/file-path request "/favicon.ico") :rel "shortcut icon" :type "image/vnd.microsoft.icon"}]
     [:title (head-title (:title page))]]
    [:body
-    content]))
+    content
+    [:footer "www.kodemaker.no * +47 2282 2080 * Dronning Eufemias gate 16, Visma-bygget, 0191 Oslo"]
+    [:script {:type "text/javascript"}
+     "var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-16391367-1']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();"]]))
 
 (defmethod with-layout :default [request page content]
   (html5
