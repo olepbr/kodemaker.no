5bcab383b7d1d930e88420e3d6c9cfb304f6c10f
trygvea <trygve.amundsen@gmail.com>
2018-04-07 08:28:06 +0200

    La til id på alle seksjoner på cv

--- src/kodemaker_no/pages/cv_pages.clj
+++ src/kodemaker_no/pages/cv_pages.clj
@@ -3,8 +3,8 @@
             [clojure.string :as str]
             [kodemaker-no.formatting :as f]))
 
-(defn- section [header content]
-  [:section
+(defn- section [header id content]
+  [:section {:id id}
    [:header header]
    content
    [:hr]])
@@ -39,6 +39,7 @@
 (defn- technologies [techs]
   (section
    "Teknologi"
+   "technology"
    [:dl
     (->> tech-order
          (map #(prep-tech techs %))
@@ -66,14 +67,16 @@
             (map (fn [col] [:td (col item)]) columns)])
          items)]])
 
-(defn- table-section [header items headings columns]
+(defn- table-section [header id items headings columns]
   (when (< 0 (count items))
     (section header
+             id
              (table items headings columns))))
 
-(defn- list-section [header items render-item]
+(defn- list-section [header id items render-item]
   (when (< 0 (count items))
     (section header
+             id
              [:ul
               (map (fn [item] [:li {} (render-item item)]) items)])))
 
@@ -95,6 +98,7 @@
 (defn- projects-fullview [person]
   (when (< 0 (count (:projects person)))
     (section "Prosjekter"
+             (name :projects)
              (->> (:projects person)
                   (filter :description)
                   (group-by :employer)
@@ -119,6 +123,7 @@
 (defn- open-source-contributions [{:keys [open-source-contributions]}]
   (when (< 0 (count open-source-contributions))
     (section "Bidrag til open source"
+             "open-source-contributions"
              (map render-open-source-contributions open-source-contributions))))
 
 (defn- short-fact [person attr label]
@@ -129,6 +134,7 @@
 (defn- endorsements [{:keys [endorsements]}]
   (when (< 0 (count endorsements))
     (section "Anbefalinger"
+             "endorsments"
              (map (fn [{:keys [author title quote]}]
                     (list [:h3 author (when title (list ", " title))]
                           [:blockquote quote]))
@@ -172,40 +178,48 @@
             (short-fact person :education-summary "Utdanning")
             (short-fact person :experience-since "Erfaring")]]]
          [:article
-          (section "Sammendrag" (f/to-html (:description person)))
+          (section "Sammendrag" "abstract" (f/to-html (:description person)))
           (technologies (:techs person))
           (when (< 0 (count (:qualifications person)))
             (section "Kvalifikasjoner"
+                     (name :qualifications)
                      [:ul (map #(vector :li %) (:qualifications person))]))
           (table-section "Prosjekter, sammendrag"
+                         (name :projects)
                          (:projects person)
                          ["Oppdragsgiver" "Periode" "Oppdrag"]
                          [:customer year-range :summary])
           (projects-fullview person)
           (table-section "Foredrag/kurs"
+                         (name :appearances)
                          (:appearances person)
                          ["Navn" "Sted" "Når"]
                          [linked-title :event :year-month])
           (open-source-contributions person)
           (list-section "Andre faglige bidrag"
+                        (name :other)
                         (:other person)
                         (fn [{:keys [title url summary]}]
                           (if url
                             (list [:a {:href url} title] " - " (f/to-html summary))
                             (list (when title (str title " - ")) (f/to-html summary)))))
           (table-section "Utdanning"
+                         (name :education)
                          (:education person)
                          ["Skole" "År" "Retning"]
                          [:institution year-range :subject])
           (table-section "Sertifiseringer/ kurs"
+                         (name :certifications)
                          (:certifications person)
                          ["Kursnavn" "År" ""]
                          [certification-name :year certificate])
           (table-section "Domenekunnskap"
+                         (name :domain-skills)
                          (:domain-skills person)
                          ["Område" "Kjennskap"]
                          [:title :description])
           (table-section "Språk"
+                         (name :languages)
                          (:languages person)
                          ["Språk" "Muntlig" "Skriftlig"]
                          [:language :orally :written])
