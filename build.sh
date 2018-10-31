9e4d7c2ea8bd89ab760952046db1ecf5bd0b22c5
Christian Johansen <christian@cjohansen.no>
2018-10-31 23:45:49 +0100

    Take #3 på deployment fra Travis

--- build.sh
+++ build.sh
@@ -16,12 +16,10 @@ role_creds=$(aws sts assume-role \
                  --role-arn "arn:aws:iam::195221715009:role/Deployer" \
                  --role-session-name DeployKodemakerWeb)
 
-prod-aws() {
-  env \
-      AWS_ACCESS_KEY_ID=$(echo "$role_creds" | jq -cr .Credentials.AccessKeyId) \
-      AWS_SECRET_ACCESS_KEY=$(echo "$role_creds" | jq -cr .Credentials.SecretAccessKey) \
-      AWS_SESSION_TOKEN=$(echo "$role_creds" | jq -cr .Credentials.SessionToken) \
-      aws $@
+prod-env() {
+  echo "AWS_ACCESS_KEY_ID=$(echo "$role_creds" | jq -cr .Credentials.AccessKeyId)"
+  echo "AWS_SECRET_ACCESS_KEY=$(echo "$role_creds" | jq -cr .Credentials.SecretAccessKey)"
+  echo "AWS_SESSION_TOKEN=$(echo "$role_creds" | jq -cr .Credentials.SessionToken)"
 }
 
 bucket="s3://kodemaker-www/"
@@ -30,14 +28,14 @@ target="build"
 echo "Syncing down production site for accurate build diffs"
 mkdir -p "$target"
 pushd "$target" > /dev/null
-prod-aws s3 sync $bucket .
+env $(prod-env) aws s3 sync $bucket .
 popd > /dev/null
 
 echo "Building site"
 diffs=$(lein build-site :json)
 
 echo "Generating PDFs"
-$(aws ecr get-login --no-include-email)
+$(aws ecr get-login --region eu-west-1 --no-include-email)
 docker run --rm -v $(cd $(dirname $0)/build && pwd):/site 575778107697.dkr.ecr.eu-west-1.amazonaws.com/html2pdf:130bd62355 /site/cv /site
 
 echo "Syncing assets, cacheable for a year"
@@ -50,13 +48,13 @@ if [ $? -ne 0 ]; then
 fi
 
 expires="$(format-date $ts "%a, %d %b %Y %H:%M:%S GMT")"
-prod-aws s3 sync . $bucket --expires "$expires" --exclude "*" --include "assets/*"
+env $(prod-env) aws s3 sync . $bucket --expires "$expires" --exclude "*" --include "assets/*"
 
 echo "Syncing remaining files and deleting removed files"
-prod-aws s3 sync . $bucket --delete --exclude "assets/*"
+env $(prod-env) aws s3 sync . $bucket --delete --exclude "assets/*"
 
 echo "Purging old assets"
-prod-aws s3 sync . $bucket --expires "$expires" --exclude "*" --include "assets/*" --delete
+env $(prod-env) aws s3 sync . $bucket --expires "$expires" --exclude "*" --include "assets/*" --delete
 
 changed=$(for file in $(echo "${diffs}" | jq -cr '.changed[]'); do
             echo ${file/"index.html"/""}
@@ -69,5 +67,5 @@ removed=$(for file in $(echo "${diffs}" | jq -cr '.removed[]'); do
 if [ "$changed$removed" != "" ]; then
   paths=$(echo "$changed $removed")
   echo "Purging Cloudfront caches for $paths"
-  prod-aws cloudfront create-invalidation --distribution-id E377BQUYES9DH7 --paths $paths
+  env $(prod-env) aws cloudfront create-invalidation --distribution-id E377BQUYES9DH7 --paths $paths
 fi
