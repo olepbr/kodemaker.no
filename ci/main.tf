8c7eba54aa260127db93aba5a8487e71b473ae19
Christian Johansen <christian@cjohansen.no>
2018-11-01 10:09:26 +0100

    Cache lengre! Vi purger ved deploy uansett

--- ci/main.tf
+++ ci/main.tf
@@ -128,8 +128,8 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
 
     viewer_protocol_policy = "allow-all"
     min_ttl = 0
-    default_ttl = 3600
-    max_ttl = 86400
+    default_ttl = 86400
+    max_ttl = 604800
     compress = true
     viewer_protocol_policy = "allow-all"
 
