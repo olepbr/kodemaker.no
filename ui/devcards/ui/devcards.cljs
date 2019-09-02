491ad4728c76a96e20cfd04d6cece220b0a1f055
Christian Johansen <christian@cjohansen.no>
2019-09-02 14:05:52 +0200

    Require the devcards ns before using it

--- ui/devcards/ui/devcards.cljs
+++ ui/devcards/ui/devcards.cljs
@@ -1,10 +1,11 @@
 (ns ^:figwheel-hooks ui.devcards
-  (:require [ui.typo-cards]))
+  (:require [ui.typo-cards]
+            [devcards.core :as devcards]))
 
 (enable-console-print!)
 
 (defn render []
-  (devcards.core/start-devcard-ui!))
+  (devcards/start-devcard-ui!))
 
 (defn ^:after-load render-on-relaod []
   (render))
