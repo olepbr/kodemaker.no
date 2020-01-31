7e24612eded80f739c53c8d4816bbef47d43e538
Magnar Sveen <magnars@gmail.com>
2020-01-31 09:21:18 +0100

    Rydd litt i forsidetekstene

--- src/kodemaker_no/new_pages/frontpage.clj
+++ src/kodemaker_no/new_pages/frontpage.clj
@@ -2,12 +2,12 @@
 
 (defn create-page []
   {:sections [{:kind :bruce
-               :title "Et unikt team av señiorutviklere"
+               :title "Et unikt team av seniorutviklere"
                :text "Vi håndplukker erfarne konsulenter som er selvgående og
                       trygge på sine meninger. Våre folk har sterkt fokus på
                       kompetanseoverføring i team og tar komplekse oppgaver med
                       tunge integrasjoner."
-               :link {:text "Jobb med oss"
+               :link {:text "Hvem er Kodemaker?"
                       :href "/jobb/"}
                :image-front "/bruce-front/foto/christian-1.jpg"
                :image-back "/bruce-back/foto/nils-1.jpg"}
@@ -20,17 +20,17 @@
                :seymours
                [{:icon {:type :science/chemical :height 79}
                  :title "Referanser"
-                 :text "Det er fleske meg ikke dårlig hvor mange artige prosjekter vi har fått være med på."
+                 :text "Vi har kun erfarne konsulenter med oss som liker å bryne seg på vanskelige oppgaver."
                  :link {:text "Se referanser"
                         :href "/referanser/"}}
                 {:icon {:type :science/robot-1 :height 79}
-                 :title "Sjekk ut hvem vi har på laget 'æ, guttær!"
+                 :title "Se hvem vi har på laget"
                  :text "Vi har kun erfarne konsulenter med oss som liker å bryne seg på vanskelig oppgaver."
                  :link {:text "Våre ansatte"
                         :href "/folk/"}}
                 {:icon {:type :computer/laptop-1 :height 79}
                  :title "Kurs og workshops"
-                 :text "Her er en ganske kort tekst."
+                 :text ""
                  :link {:text "Vi kan tilby"
                         :href "/kurs/"}}]}
               {:kind :vertigo
