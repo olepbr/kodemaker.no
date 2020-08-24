65bfbd7c3e53b39651cf1997e47693f21364e363
Christian Johansen <christian@cjohansen.no>
2020-08-24 12:12:01 +0200

    Pek både kodemaker.no og www.kodemaker.no til Cloudfront, redirect til HTTPS alltid

--- ci/main.tf
+++ ci/main.tf
@@ -1,6 +1,7 @@
 locals {
-  domain_name = "www.kodemaker.no"
   secondary_domain_name = "kodemaker.no"
+
+  domain_name = "kodemaker.no"
   s3_origin_id = "StaticFilesS3BucketOrigin"
 }
 
@@ -19,7 +20,7 @@ resource "aws_s3_bucket" "bucket" {
 }
 
 resource "aws_cloudfront_origin_access_identity" "identity" {
-  comment = "Origin access identity for ${local.domain_name}"
+  comment = "Origin access identity for www.${local.domain_name}"
 }
 
 data "aws_iam_policy_document" "s3_policy" {
@@ -64,7 +65,7 @@ data "aws_iam_policy_document" "lambda" {
 }
 
 resource "aws_iam_role" "lambda_role" {
-  name_prefix = "${local.domain_name}"
+  name_prefix = "www.${local.domain_name}"
   assume_role_policy = "${data.aws_iam_policy_document.lambda.json}"
 }
 
@@ -98,7 +99,8 @@ resource "aws_lambda_function" "basic_auth" {
 
 resource "aws_acm_certificate" "cert" {
   provider = "aws.us-east-1"
-  domain_name = "www.kodemaker.no"
+  domain_name = "${local.domain_name}"
+  subject_alternative_names = ["www.${local.domain_name}"]
   validation_method = "DNS"
 
   lifecycle {
@@ -106,23 +108,7 @@ resource "aws_acm_certificate" "cert" {
   }
 }
 
-resource "aws_route53_record" "cert_validation" {
-  name = "${aws_acm_certificate.cert.domain_validation_options.0.resource_record_name}"
-  type = "${aws_acm_certificate.cert.domain_validation_options.0.resource_record_type}"
-  zone_id = "${data.aws_route53_zone.zone.id}"
-  records = ["${aws_acm_certificate.cert.domain_validation_options.0.resource_record_value}"]
-  ttl = 60
-}
-
-resource "aws_acm_certificate_validation" "cert" {
-  provider = "aws.us-east-1"
-  certificate_arn = "${aws_acm_certificate.cert.arn}"
-  validation_record_fqdns = ["${aws_route53_record.cert_validation.fqdn}"]
-}
-
 resource "aws_cloudfront_distribution" "s3_distribution" {
-  depends_on = ["aws_acm_certificate_validation.cert"]
-
   origin {
     domain_name = "${aws_s3_bucket.bucket.bucket_regional_domain_name}"
     origin_id = "${local.s3_origin_id}"
@@ -140,9 +126,12 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
 
   enabled = true
   is_ipv6_enabled = true
-  comment = "Distribution for ${local.domain_name}"
+  comment = "Distribution for www.${local.domain_name}"
   default_root_object = "index.html"
-  aliases = ["${local.domain_name}"]
+  aliases = [
+    "${local.domain_name}",
+    "www.${local.domain_name}"
+  ]
 
   default_cache_behavior {
     allowed_methods  = ["GET", "HEAD", "OPTIONS"]
@@ -157,12 +146,11 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
       }
     }
 
-    viewer_protocol_policy = "allow-all"
     min_ttl = 0
     default_ttl = 86400
     max_ttl = 604800
     compress = true
-    viewer_protocol_policy = "allow-all"
+    viewer_protocol_policy = "redirect-to-https"
 
     lambda_function_association {
       event_type = "viewer-request"
@@ -181,7 +169,7 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
     default_ttl = 86400
     max_ttl = 31536000
     compress = true
-    viewer_protocol_policy = "allow-all"
+    viewer_protocol_policy = "redirect-to-https"
 
     forwarded_values {
       query_string = false
@@ -219,23 +207,14 @@ resource "aws_route53_record" "www_record" {
   }
 }
 
-resource "aws_s3_bucket" "redirect" {
-  bucket = "${local.secondary_domain_name}"
-  acl = "public-read"
-
-  website {
-    redirect_all_requests_to = "https://${local.domain_name}"
-  }
-}
-
 resource "aws_route53_record" "root_record" {
-  name = "${local.secondary_domain_name}"
+  name = "www.${local.domain_name}"
   zone_id = "${data.aws_route53_zone.zone.zone_id}"
   type = "A"
 
   alias {
-    name = "${aws_s3_bucket.redirect.website_domain}"
-    zone_id = "${aws_s3_bucket.redirect.hosted_zone_id}"
+    name = "${aws_cloudfront_distribution.s3_distribution.domain_name}"
+    zone_id = "${aws_cloudfront_distribution.s3_distribution.hosted_zone_id}"
     evaluate_target_health = true
   }
 }
