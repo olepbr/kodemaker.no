36a515baa420a9929075e49efdd374ed9fcc1a98
Christian Johansen <christian@cjohansen.no>
2020-08-25 09:52:07 +0200

    Trekk ut video-grid som et element og fiks formattering av profilkoden

--- src/kodemaker_no/new_pages/profile_page.clj
+++ src/kodemaker_no/new_pages/profile_page.clj
@@ -16,81 +16,98 @@
 (defn any-technology-categories? [person]
   (boolean (some (fn [[k _]] (seq (k person))) technology-categories)))
 
+(defn video-grid [items]
+  (e/tango-grid
+   (map
+    (fn [{:keys [image tech-list url title]} style video-class grid-class]
+      {:class grid-class
+       :content (e/video-thumb
+                 {:class (str style " " video-class)
+                  :img (str "/" style "/" image)
+                  :tags (e/tech-tags {:class "tags"
+                                      :techs (take 5 tech-list)})
+                  :url url
+                  :title title})})
+    (take 4 items)
+    ["video-thumb-rouge" "video-thumb-chocolate" "video-thumb-chocolate" "video-thumb-rouge"]
+    ["curtain curtain-short-right" nil nil "curtain curtain-short-top"]
+    [nil nil "hide-below-600" "hide-below-600"])))
+
 (defn create-page [person]
   (let [cv-uri (:page/uri (:cv/_person person))]
     {:title (:person/full-name person)
-   :sections
-   (->>
-    [{:kind :profile
-      :full-name (:person/full-name person)
-      :image (:person/profile-page-picture person)
-      :title (:person/title person)
-      :mobile (:person/phone-number person)
-      :mail (:person/email-address person)
-         :cv (when cv-uri
-               {:text "Se full CV"
-                :url cv-uri})
-      :description (f/markdown (:person/description person))
-      :presence (person/prep-presence-links (:person/presence person))
-      :pønt [{:kind :greater-than
-              :position "top -270px left 12%"}
-             {:kind :dotgrid
-              :position "bottom -150px right -150px"}]}
+     :sections
+     (->>
+      [{:kind :profile
+        :full-name (:person/full-name person)
+        :image (:person/profile-page-picture person)
+        :title (:person/title person)
+        :mobile (:person/phone-number person)
+        :mail (:person/email-address person)
+        :cv (when cv-uri
+              {:text "Se full CV"
+               :url cv-uri})
+        :description (f/markdown (:person/description person))
+        :presence (person/prep-presence-links (:person/presence person))
+        :pønt [{:kind :greater-than
+                :position "top -270px left 12%"}
+               {:kind :dotgrid
+                :position "bottom -150px right -150px"}]}
 
-     ;; Teknologi
+       ;; Teknologi
 
-     (when (any-technology-categories? person)
-       {:kind :enumeration
-        :title "Teknologi"
-        :categories (->> (for [[k label] technology-categories]
-                           {:label label
-                            :items (for [tech (unwrap-idents person k)]
-                                     {:text (:tech/name tech)
-                                      :href (:page/uri tech)})})
-                         (remove (comp empty? :items)))
-        :pønt [{:kind :dotgrid
-                :position "top -344px right -150px"}]})
+       (when (any-technology-categories? person)
+         {:kind :enumeration
+          :title "Teknologi"
+          :categories (->> (for [[k label] technology-categories]
+                             {:label label
+                              :items (for [tech (unwrap-idents person k)]
+                                       {:text (:tech/name tech)
+                                        :href (:page/uri tech)})})
+                           (remove (comp empty? :items)))
+          :pønt [{:kind :dotgrid
+                  :position "top -344px right -150px"}]})
 
-     ;; Anbefalinger
+       ;; Anbefalinger
 
-     (when-let [recommendations (seq (:person/recommendations person))]
-       {:kind :titled
-        :title (str (f/genitive-name (:person/given-name person)) " anbefalinger")
-        :contents (for [recommendation (take 3 (sort-by :list/idx recommendations))]
-                    (e/teaser
-                     (cond-> {:title (:recommendation/title recommendation)
-                              :tags (e/tech-tags {:techs (take 5 (h/unwrap-ident-list recommendation :recommendation/tech-list))
-                                                  :class "tags"})
-                              :url (:recommendation/url recommendation)
-                              :content (f/to-html (:recommendation/description recommendation))}
-                       (:recommendation/link-text recommendation)
-                       (assoc :link {:text (:recommendation/link-text recommendation)
-                                     :href (:recommendation/url recommendation)}))))})
-
-     ;; Snakker gjerne om
-
-     (when-let [[uno dos] (seq (sort-by :list/idx (:person/hobbies person)))]
-       (let [hobby-1 {:title (:title uno)
-                      :content (f/to-html (:description uno))
-                      :image (str "/big-bottom-half-circle" (:illustration uno))}
-             hobby-2 (when dos
-                       {:title (:title dos)
-                        :content (f/to-html (:description dos))
-                        :image (str "/hobby-square" (:illustration dos))})]
-         {:kind :hip
-          :title (str (:person/given-name person) " snakker gjerne om")
-          (if hobby-2 :left :single) hobby-1
-          :right hobby-2}))
-
-     ;; Bloggposter
-
-     (when-let [blog-posts (seq (:blog-post/_author person))]
-       {:kind :titled
-        :title "Bloggposter"
-        :contents (for [blog-post (take 3 (reverse (sort-by :blog-post/published blog-posts)))]
-                    (let [url (or (:page/uri blog-post)
-                                  (:blog-post/external-url blog-post))]
+       (when-let [recommendations (seq (:person/recommendations person))]
+         {:kind :titled
+          :title (str (f/genitive-name (:person/given-name person)) " anbefalinger")
+          :contents (for [recommendation (take 3 (sort-by :list/idx recommendations))]
                       (e/teaser
+                       (cond-> {:title (:recommendation/title recommendation)
+                                :tags (e/tech-tags {:techs (take 5 (h/unwrap-ident-list recommendation :recommendation/tech-list))
+                                                    :class "tags"})
+                                :url (:recommendation/url recommendation)
+                                :content (f/to-html (:recommendation/description recommendation))}
+                         (:recommendation/link-text recommendation)
+                         (assoc :link {:text (:recommendation/link-text recommendation)
+                                       :href (:recommendation/url recommendation)}))))})
+
+       ;; Snakker gjerne om
+
+       (when-let [[uno dos] (seq (sort-by :list/idx (:person/hobbies person)))]
+         (let [hobby-1 {:title (:title uno)
+                        :content (f/to-html (:description uno))
+                        :image (str "/big-bottom-half-circle" (:illustration uno))}
+               hobby-2 (when dos
+                         {:title (:title dos)
+                          :content (f/to-html (:description dos))
+                          :image (str "/hobby-square" (:illustration dos))})]
+           {:kind :hip
+            :title (str (:person/given-name person) " snakker gjerne om")
+            (if hobby-2 :left :single) hobby-1
+            :right hobby-2}))
+
+       ;; Bloggposter
+
+       (when-let [blog-posts (seq (:blog-post/_author person))]
+         {:kind :titled
+          :title "Bloggposter"
+          :contents (for [blog-post (take 3 (reverse (sort-by :blog-post/published blog-posts)))]
+                      (let [url (or (:page/uri blog-post)
+                                    (:blog-post/external-url blog-post))]
+                        (e/teaser
                          {:title (:blog-post/title blog-post)
                           :tags (e/tech-tags {:techs (take 5 (h/unwrap-ident-list blog-post :blog-post/tech-list))
                                               :class "tags"})
@@ -107,22 +124,16 @@
                                      seq)]
          {:kind :titled
           :title "Foredrag"
-          :contents [(e/tango-grid
-                      (map
-                       (fn [pres style video-class grid-class]
-                         {:class grid-class
-                          :content (e/video-thumb
-                                    {:class (str style " " video-class)
-                                     :img (str "/" style "/" (:presentation/thumb pres))
-                                     :tags (e/tech-tags {:class "tags"
-                                                         :techs (take 5 (h/unwrap-ident-list pres :presentation/tech-list))})
-                                     :url (or (:page/uri pres)
-                                              (:presentation/video-url pres))
-                                     :title (:presentation/title pres)})})
-                       (take 4 (reverse (sort-by :presentation/date presentations)))
-                       ["video-thumb-rouge" "video-thumb-chocolate" "video-thumb-chocolate" "video-thumb-rouge"]
-                       ["curtain curtain-short-right" nil nil "curtain curtain-short-top"]
-                       [nil nil "hide-below-600" "hide-below-600"]))]})
+          :contents [(video-grid
+                      (->> (sort-by :presentation/date presentations)
+                           reverse
+                           (map (fn [pres]
+                                  {:image (:presentation/thumb pres)
+                                   :tech-list (h/unwrap-ident-list pres :presentation/tech-list)
+                                   :title (:presentation/title pres)
+                                   :url (or (:page/uri pres)
+                                            (:presentation/video-url pres))}))))]})
+
        ;; Screencasts
 
        (when-let [screencasts (seq (:person/screencasts person))]
