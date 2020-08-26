a255bb40c2da47d334ba5eb1104c94dc872e0d87
Christian Johansen <christian@cjohansen.no>
2020-08-26 12:36:06 +0200

    Trekk ut hver enkelt seksjon på tech-sida som en egen funksjon

--- src/kodemaker_no/new_pages/tech_page.clj
+++ src/kodemaker_no/new_pages/tech_page.clj
@@ -38,16 +38,31 @@
                          :recommendation/description first})
       (assoc :recommendation/people (keep :person/_recommendations recommendations))))
 
-(defn create-page [tech]
-  (let [merged-presentations (->> (:presentation/_techs tech)
-                                  (group-by presentation-uri)
-                                  vals
-                                  (map merge-presentations))
-        merged-recommendations (->> (:recommendation/_techs tech)
-                                    (group-by :recommendation/url)
-                                    vals
-                                    (map merge-recommendations))
-        all-presentations (->> merged-presentations
+(defn recommendations-section [tech]
+  (when-let [merged-recommendations (->> (:recommendation/_techs tech)
+                                         (group-by :recommendation/url)
+                                         vals
+                                         (map merge-recommendations)
+                                         seq)]
+    {:kind :titled
+     :title "Våre anbefalinger"
+     :contents (for [recommendation (->> merged-recommendations
+                                         (sort-by (juxt (comp - count :recommendation/people)
+                                                        (comp - count :recommendation/description)))
+                                         (take 5))]
+                 (e/teaser
+                  {:title (:recommendation/title recommendation)
+                   :tags (e/people-tags {:prefix "Anbefalt av"
+                                         :people (:recommendation/people recommendation)
+                                         :class "tags"})
+                   :url (:recommendation/url recommendation)
+                   :content (f/to-html (:recommendation/description recommendation))}))}))
+
+(defn classify-presentations [tech]
+  (let [all-presentations (->> (:presentation/_techs tech)
+                               (group-by presentation-uri)
+                               vals
+                               (map merge-presentations)
                                (filter #(< 80 (count (:presentation/description % ""))))
                                (sort-by :presentation/date)
                                (reverse)
@@ -55,7 +70,49 @@
         presentation-stars (->> all-presentations
                                 (filter :presentation/thumb)
                                 (filter :presentation/video-url))
-        headliner (when (:tech/description tech) (first presentation-stars))]
+        headliner (when (:tech/description tech)
+                    (first presentation-stars))
+        to-show-in-grid (take 4 (remove #{headliner} presentation-stars))]
+    {:headliner headliner
+     :to-show-in-grid to-show-in-grid
+     :remaining (remove (into #{headliner} to-show-in-grid)
+                        all-presentations)}))
+
+(defn presentations-section [{:keys [to-show-in-grid remaining]}]
+  (when (seq (concat to-show-in-grid remaining))
+    {:kind :titled
+     :title "Våre foredrag"
+     :contents (->> [(when (seq to-show-in-grid)
+                       (e/tango-grid
+                        (map
+                         (fn [pres style video-class]
+                           {:content [:div
+                                      (e/video-thumb
+                                       {:class (str style " " video-class)
+                                        :img (str "/" style "/" (:presentation/thumb pres))
+                                        :tags (e/people-tags {:class "tags"
+                                                              :people (:presentation/people pres)})
+                                        :url (presentation-uri pres)
+                                        :title (:presentation/title pres)})
+                                      [:div.text (f/to-html (:presentation/description pres))]]})
+                         to-show-in-grid
+                         ["video-thumb-rouge" "video-thumb-chocolate" "video-thumb-chocolate" "video-thumb-rouge"]
+                         ["curtain curtain-short-right" nil nil "curtain curtain-short-top"])))
+                     (when (seq remaining)
+                       [:div
+                        (for [pres remaining]
+                          [:div.mbm
+                           [:a.link {:href (presentation-uri pres)}
+                            (:presentation/title pres)]
+                           [:div
+                            (e/people-tags {:class "tags"
+                                            :people (:presentation/people pres)})]
+                           [:div.text
+                            (f/to-html (:presentation/description pres))]])])]
+                    (remove nil?))}))
+
+(defn create-page [tech]
+  (let [presentations (classify-presentations tech)]
     {:title (:tech/name tech)
      :sections
      (->>
@@ -66,7 +123,7 @@
         :article {:content [:div.text
                             (f/to-html (:tech/description tech))]
                   :alignment :front
-                  :aside (when headliner
+                  :aside (when-let [headliner (:headliner presentations)]
                            [:div.hide-below-1000
                             (e/video-thumb
                              {:img (str "/rouge-duotone/" (:presentation/thumb headliner))
@@ -80,64 +137,10 @@
                {:kind :dotgrid
                 :position "top -110px left 80vw"}]}
 
-       ;; Anbefalinger
-
-       (when (seq merged-recommendations)
-         {:kind :titled
-          :title "Våre anbefalinger"
-          :contents (for [recommendation (->> merged-recommendations
-                                              (sort-by (juxt (comp - count :recommendation/people)
-                                                             (comp - count :recommendation/description)))
-                                              (take 5))]
-                      (e/teaser
-                       {:title (:recommendation/title recommendation)
-                        :tags (e/people-tags {:prefix "Anbefalt av"
-                                              :people (:recommendation/people recommendation)
-                                              :class "tags"})
-                        :url (:recommendation/url recommendation)
-                        :content (f/to-html (:recommendation/description recommendation))}))})
-
-       ;; Foredrag
-
-       (when all-presentations
-         (let [to-show-in-grid (take 4 (remove #{headliner} presentation-stars))
-               remaining (remove (into #{headliner} to-show-in-grid)
-                                 all-presentations)]
-           {:kind :titled
-            :title "Våre foredrag"
-            :contents (->> [(when (seq to-show-in-grid)
-                              (e/tango-grid
-                               (map
-                                (fn [pres style class]
-                                  {:content [:div
-                                             (e/video-thumb
-                                              {:class (str style " " class)
-                                               :img (str "/" style "/" (:presentation/thumb pres))
-                                               :tags (e/people-tags {:class "tags"
-                                                                     :people (:presentation/people pres)})
-                                               :url (presentation-uri pres)
-                                               :title (:presentation/title pres)})
-                                             [:div.text
-                                              (f/to-html (:presentation/description pres))]]})
-                                to-show-in-grid
-                                ["video-thumb-rouge" "video-thumb-chocolate" "video-thumb-chocolate" "video-thumb-rouge"]
-                                ["curtain curtain-short-right" nil nil "curtain curtain-short-top"])))
-                            (when (seq remaining)
-                              [:div
-                               (for [pres remaining]
-                                 [:div.mbm
-                                  [:a.link {:href (presentation-uri pres)}
-                                   (:presentation/title pres)]
-                                  [:div
-                                   (e/people-tags {:class "tags"
-                                                   :people (:presentation/people pres)})]
-                                  [:div.text
-                                   (f/to-html (:presentation/description pres))]])])]
-                           (remove nil?))}))
+       (recommendations-section tech)
+       (presentations-section presentations)
 
        ;; Bloggposter
-
-
 
        {:kind :footer}]
       (remove nil?)
