61591e664d7889bbecfa9ad7582f3ec8f4d227b3
Magnar Sveen <magnars@gmail.com>
2020-08-28 22:14:30 +0200

    Vi er ikke miljøerfarne utviklere

--- src/kodemaker_no/new_pages/frontpage.clj
+++ src/kodemaker_no/new_pages/frontpage.clj
@@ -2,7 +2,7 @@
 
 (defn create-page []
   {:sections [{:kind :bruce
-               :title "Engasjert miljø - erfarne utviklere"
+               :title "Engasjert miljø, erfarne utviklere"
                :text "Vi er ansvarlige, engasjerte og uformelle konsulenter. Vi er selvgående, trygge på våre meninger og utfordrer gjerne det eksisterende. Er du en av de som vet at et fåtall dyktige utviklere kan utgjøre en stor forskjell, så ta gjerne kontakt med oss."
                :link {:text "Ta kontakt"
                       :href "/kontakt/"}
