7f3bb581c23f59948cbbc2259ca676cb8e7e075d
Christian Johansen <christian@cjohansen.no>
2022-09-04 12:24:08 +0200

    hawk doesn't work well on M1 Macs, replace with beholder

--- dev/kodemaker_no/ingestion_watcher.clj
+++ dev/kodemaker_no/ingestion_watcher.clj
@@ -2,34 +2,32 @@
   (:require [clojure.java.io :as io]
             [clojure.string :as str]
             [datomic-type-extensions.api :as d]
-            [hawk.core :as hawk]
             [kodemaker-no.files :as files]
             [kodemaker-no.formatting :as f]
-            [kodemaker-no.ingest :as ingest]))
+            [kodemaker-no.ingest :as ingest]
+            [nextjournal.beholder :as beholder]))
 
 (defn start! [directory conn]
   (let [file (io/file directory)
         chop-length (inc (count (.getAbsolutePath file)))]
-    (hawk/watch!
-     [{:paths [directory]
-       :filter hawk/file?
-       :handler
-       (fn [_ e]
-         (let [file-path (subs (.getAbsolutePath (:file e)) chop-length)]
-           (when (ingest/ingest conn (files/normalize-path file-path))
-             (ingest/perform-last-minute-changes conn)
-             (when-let [techs (seq (ingest/find-uncategorized-techs (d/db conn)))]
-               (println
-                (format "[watcher] Det har sneket seg inn techs som ikke er kategorisert! Sørg for å enten fikse stavingen av %s (fra %s) eller legg %s til i resources/tech-categories."
-                        (str/join (f/comma-separated (map first techs)))
-                        (str/join (f/comma-separated (set (map second techs))))
-                        (if (< 1 (count techs)) "dem" "den"))))
-             (println "[watcher]"
-                      (case (:kind e)
-                        :create "Ingested"
-                        :modify "Updated"
-                        :delete "Removed")
-                      file-path))))}])))
+    (beholder/watch
+     (fn [{:keys [type path]}]
+       (let [file-path (subs (.getAbsolutePath (.toFile path)) chop-length)]
+         (when (ingest/ingest conn (files/normalize-path file-path))
+           (ingest/perform-last-minute-changes conn)
+           (when-let [techs (seq (ingest/find-uncategorized-techs (d/db conn)))]
+             (println
+              (format "[watcher] Det har sneket seg inn techs som ikke er kategorisert! Sørg for å enten fikse stavingen av %s (fra %s) eller legg %s til i resources/tech-categories."
+                      (str/join (f/comma-separated (map first techs)))
+                      (str/join (f/comma-separated (set (map second techs))))
+                      (if (< 1 (count techs)) "dem" "den"))))
+           (println "[watcher]"
+                    (case type
+                      :create "Ingested"
+                      :modify "Updated"
+                      :delete "Removed"
+                      :overflow "Overflowed(?)")
+                    file-path)))) directory)))
 
 (defn stop! [watcher]
-  (hawk/stop! watcher))
+  (beholder/stop watcher))
